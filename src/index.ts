5b9ba04b6488d200811677cc31bad82b5f88ba94
Kelvin Zhang <zhangxp1998@gmail.com>
2019-11-05 13:47:07 -0500

    Hide label icons in google map

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,23 @@ import * as d3 from 'd3';
 import * as a from '../data/IL_minified.json';
 
 function initMap() {
+    const labelsOff: google.maps.MapTypeStyle[] = [
+        {
+            elementType: "labels.icon",
+            featureType: "all",
+            stylers: [
+                { visibility: "off" }
+            ]
+        }
+    ];
     let map = new google.maps.Map(document.getElementById("google-map"), {
         zoom: 4,
-        center: {lat: 40.354731, lng: -100.75564},
+        center: { lat: 40.354731, lng: -100.75564 },
         streetViewControl: false,
         mapTypeControl: false,
-      });
+        styles: labelsOff,
+    });
+
     console.log(map.getBounds());
 }
 
