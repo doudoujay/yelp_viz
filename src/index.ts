f4950f5e4e0c97829a2536920ae909640f3b4a36
Kelvin Zhang <zhangxp1998@gmail.com>
2019-11-12 10:30:54 -0500

    Add edges to map

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
 import * as d3 from 'd3';
-import * as businesses from '../data/IL_minified.json';
+import * as raw_businesses from '../data/IL_minified.json';
+import * as raw_edges from '../data/IL_edges.json'
+
+interface Node {
+    business_id: string,
+    name: string,
+    address: string,
+    city: string,
+    state: string,
+    postal_code: string,
+    lat : number,
+    lng : number,
+    stars : number,
+    review_count : number,
+    categories: Array<string>,
+    links?: Array<google.maps.Polyline>,
+}
+
+interface Edge {
+    src: string,
+    dst: string,
+    data: Array<string>,
+    source?: Node,
+    destination?: Node,
+}
+
+let businesses = raw_businesses as Array<Node>
+let edges = raw_edges as Array<Edge>;
 
 function createColorScale(keys: Array<string> | Set<string>) {
     if (!Array.isArray(keys)) {
@@ -13,6 +40,20 @@ function createColorScale(keys: Array<string> | Set<string>) {
         .domain([0, keys.length]);
 
     return (d: string) => linear(ordinal(d));
+}
+
+function preprocess() {
+    let id_to_idx: Record<string, Node> = {}
+    businesses.forEach(d => {
+        id_to_idx[d.business_id] = d;
+        d.links = [];
+    });
+    edges.forEach(e => {
+        e.source = id_to_idx[e.src];
+        e.destination = id_to_idx[e.dst];
+    });
+    (<any>window).edges = edges;
+    (<any>window).businesses = businesses;
 }
 
 function initMap() {
@@ -34,27 +75,29 @@ function initMap() {
     });
     (<any>window).map = map;
 
-    const uniqueStates = new Set(businesses.map(d => d.city));
     let colorScale = d3.scaleSequential(d3.interpolateHcl("yellow", "red"))
+        .domain(d3.extent(businesses, d => d.stars));
+
+    let sizeScale = d3.scaleSqrt()
         .domain(d3.extent(businesses, d => d.review_count))
+        .range([0, 20]);
+    let meters_per_px = 156543.03392 / Math.pow(2, map.getZoom());
     let nodes = businesses.map(node => {
         let circle = new google.maps.Circle({
             strokeColor: "white",
             strokeOpacity: 1,
             strokeWeight: 1,
-            fillColor: colorScale(node.review_count),
-            fillOpacity: 0.8,
+            fillColor: colorScale(node.stars),
+            fillOpacity: 0.7,
             map: map,
-            center: {lat: node.latitude, lng: node.longitude},
-            radius: node.stars * 10,
+            center: node,
+            radius: sizeScale(node.review_count) * meters_per_px,
             zIndex: -1,
         });
         circle.set('data', node);
         return circle;
     });
-    let sizeScale = d3.scaleSqrt()
-        .domain(d3.extent(businesses, d => d.review_count))
-        .range([0, 20]);
+
     map.addListener('zoom_changed', _ => {
         // Adapted from https://medium.com/techtrument/how-many-miles-are-in-a-pixel-a0baf4611fff
         let meters_per_px = 156543.03392 / Math.pow(2, map.getZoom());
@@ -62,7 +105,22 @@ function initMap() {
             node.setRadius(sizeScale((node.get('data')).review_count)*meters_per_px);
         });
     });
+
+    let links = edges.filter(d => d.data.length >= 7).map(link => {
+        let l = new google.maps.Polyline({
+            path: [link.source, link.destination],
+            map: map,
+            strokeColor: 'red',
+            strokeWeight: 1,
+            zIndex: -2,
+            strokeOpacity: 0.7,
+        });
+        link.source.links.push(l);
+        link.destination.links.push(l);
+        return l;
+    });
 }
 
 (<any>window).initMap = initMap;
+preprocess();
 console.log("Function", initMap, " should be called after Google Map is loaded.");
