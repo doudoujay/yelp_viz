66bf98fe1fb4dc713d6dd0ded40a031528a8dc23
Kelvin Zhang <zhangxp1998@gmail.com>
2019-11-14 13:21:37 -0500

    Color code edges in force layout

--- src/force.ts
+++ src/force.ts
@@ -17,17 +17,37 @@ export function initForceLayout(businesses: BusinessNode[], edges: Edge[], conta
         .style("width", "100%")
         .style("height", "100%");
 
+    let colorScale = d3.scaleSequential(d3.interpolateBlues)
+        .domain(d3.extent(businesses, d => d.stars));
+
+    let sizeScale = d3.scaleSqrt()
+        .domain(d3.extent(businesses, d => d.review_count))
+        .range([0, 20]);
+
     let nodes = svg.append("g").classed("nodes_container", true)
         .selectAll(".node")
         .data(businesses)
         .enter()
         .append("circle")
-        .attr("r", 3)
+        .attr("r", d => sizeScale(d.review_count))
+        .attr('fill', d => colorScale(d.stars))
         .classed('node', true);
+
+    const linkWeightRange = d3.extent(edges, d => d.data.length);
+    const linkColorScale = d3.scaleSqrt<string>()
+        .domain(linkWeightRange)
+        .range(['yellow', 'red'])
+        .interpolate(d3.interpolateHcl);
+    const linkOpacityScale = d3.scaleLog().domain(linkWeightRange).range([0.3, 1]);
+    const linkWidthScale = d3.scaleLog().domain(linkWeightRange).range([1, 5]);
+
     let links = svg.append("g").classed("links_container", true)
         .selectAll(".link")
         .data(edges)
         .enter().append("line")
+            .attr('stroke-width', d => linkWidthScale(d.data.length))
+            .attr('stroke', d => linkColorScale(d.data.length))
+            // .attr('opacity', d => linkOpacityScale(d.data.length))
             .classed("link", true);
     let x = d3.scaleLinear()
         .domain([0, 30])
